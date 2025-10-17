017bb13e234dfd4ac576762808694ec94fbcc53d
danieldev23 <huydevc23@gmail.com>
2025-10-18 02:01:09 +0700

    fix continue

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,27 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  async headers() {
+    return [
+      {
+        source: '/admin/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
